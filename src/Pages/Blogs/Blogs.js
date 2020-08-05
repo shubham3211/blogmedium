07de42c79c28ec4b8d19d81056586ebd4ee5c4ea
shubham <kumarsinghshubham32111@gmail.com>
2020-08-05 08:56:45 +0530

    changes made

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,21 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, CircularProgress } from "@material-ui/core";
 import Blog from "../../Components/Blog";
 import { useUserContext } from "../../context/UserContext";
 import { api } from "../../Utils/api";
 
 function Blogs() {
   const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { user } = useUserContext();
   useEffect(() => {
     const fetchBlogs = async () => {
+      setLoading(true);
       try {
         const response = await api.get(`/blog/${user._id}`);
         setBlogs(response.data.blogs);
-      } catch (err) {}
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+      }
     };
     fetchBlogs();
   }, [user._id]);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          paddingTop: "20%",
+          height: "100%",
+          fontSize: "200px",
+          textAlign: "center"
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
 
   return (
     <Grid container spacing={2}>
