e7637cd72ff56c68163d3593957c10868a190ca3
shubham <kumarsinghshubham32111@gmail.com>
2020-08-05 08:49:13 +0530

    changes made

--- src/Pages/CreateBlog/CreateBlog.js
+++ src/Pages/CreateBlog/CreateBlog.js
@@ -7,7 +7,7 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import CustomInput from "../../Components/input";
 import { api } from "../../Utils/api";
 import { getToken } from "../../Utils/func";
